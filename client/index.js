450bf0e54f2c49bfac64cdab3bf00bb5d45bd3e4
icyaj289 <30388879+icyaj289@users.noreply.github.com>
2019-12-17 22:20:49 +0800

    Added Express Server

---
+++ client/index.js
@@ -0,0 +1,25 @@
+function setup() {
+
+    // Initialises the toolbar.
+    toolbar = new ShootingParticles();
+
+}
+
+function draw() {
+
+    // Draws the plane.
+    toolbar.draw();
+
+}
+
+// Resize _canvas when window is re-sized
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight*.97);
+    toolbar.positionToolbar();
+}
+
+
+// creates a new particle when mouse is dragged
+function mouseDragged() {
+    toolbar.createParticle();
+}
